8f795807403bd56f5fd2c8d21a8201ce733e3969
harryleecp <e0406315@u.nus.edu>
2020-08-23 17:53:25 +0800

    Added the first line of underscores

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,8 @@
 public class Duke {
+    public static void linePrinter() {
+        System.out.println("__________________________________________");
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +10,6 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        linePrinter();
     }
 }
