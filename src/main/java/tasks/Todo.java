2de21b44ee9625cdab05127bff1153f3057b2bd9
harryleecp <e0406315@u.nus.edu>
2020-09-11 20:34:06 +0800

    Added a new LoadAndSave class that reads from and writes to a file

--- src/main/java/tasks/Todo.java
+++ src/main/java/tasks/Todo.java
@@ -1,8 +1,17 @@
 package tasks;
 
 public class Todo extends Task{
+
     public Todo(String description) {
         super(description.substring(5));
+    }
+
+    public String getTaskType() {
+        return "T";
+    }
+
+    public String getDueDate() {
+        return "";
     }
 
     public String printTask() {
