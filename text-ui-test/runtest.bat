b2e7bd282e8f6784dcfe5100ed2ca72b6c3a9c70
harryleecp <e0406315@u.nus.edu>
2020-09-20 01:07:21 +0800

    Amended the automated test batch file

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -14,8 +14,9 @@ javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\tasks\Event.ja
 javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\tasks\Todo.java
 javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\userInterface\Storage.java
 javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\userInterface\Ui.java
-javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\userInterface\Validity.java
-javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\userInterface\TaskFormatException.java
+javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\formats\Validity.java
+javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\formats\TaskFormatException.java
+javac -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\formats\DateAndTime.java
 
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
